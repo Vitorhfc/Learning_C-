31ec9696e2ebe006be0b4f78d97178c91e8e3fc7
Vitor Hugo Fernandes <105317266+Vitorhfc@users.noreply.github.com>
2023-01-19 22:46:35 -0300

    Add files via upload

---
+++ Aplicando formatação/Aplicando formatação/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+void Sistema()
+{
+    string nom_produto;
+    double valor_produto, valor_venda, lucro;
+    int x; //numero de notas;
+    static void Principal()
+    {
+        Console.WriteLine("Sistema de venda");
+        Console.WriteLine("Emissão de nota");
+        Console.WriteLine();
+        if =
+
+
+
+    }
+}
