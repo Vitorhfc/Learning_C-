9b495b601b8b0aef4d6f23c53f4ca3bc494320a2
Vitor Hugo Fernandes <105317266+Vitorhfc@users.noreply.github.com>
2023-01-19 22:34:02 -0300

    Add files via upload

---
+++ Aula_02/Aula_02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Projeto_01
+{    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demonstrando argumentos de um array");
+
+            if (args.GetLength(0) > 0)
+            {
+                Console.WriteLine(args.GetValue(0));
+            }
+            else
+            {
+                Console.WriteLine("Array vazio.");
+            }
+        }
+    }
+}
